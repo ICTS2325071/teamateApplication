e5ac6f3c8619176299bb5e8cf596be1801adfe61
DaniPosse10 <daniel.possemato@gmail.com>
2024-10-15 15:34:49 +0200

    Add files via upload

---
+++ MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.teamateapplication
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        //setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.testButton.setOnClickListener{
+
+        }
+    }
+}
